4ad2dc977ac4e2fa674c3b2f1aa4dd0c4916a523
graylum97 <luke.gray97@icloud.com>
2026-01-14 13:47:19 -0600

    generic ai agent

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+parser = argparse.ArgumentParser(description="chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+load_dotenv()
+
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("API Key not found")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents=messages
+)
+
+if response.usage_metadata is None:
+    raise RuntimeError("No usage metadata in response")
+
+usage = response.usage_metadata
+prompt_tokens = usage.prompt_token_count
+response_tokens = usage.candidates_token_count
+
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+else:
+    pass
+
+print(response.text)
+
+def main():
+    print("Hello from generic-agent!")
+
+
+if __name__ == "__main__":
+    main()
